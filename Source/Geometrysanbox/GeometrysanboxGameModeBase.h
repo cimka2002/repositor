8ab5193657ca23f4d149056b37134d34a7821af1
DESKTOP-97I8OFC\gabit <gabite20@inbox.lv>
2021-10-02 19:24:00 +0300

    First commit

---
+++ Source/Geometrysanbox/GeometrysanboxGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GeometrysanboxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEOMETRYSANBOX_API AGeometrysanboxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
